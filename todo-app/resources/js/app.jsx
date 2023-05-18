e0c276bb07dabf39f90d4c5975e76a9d9993c970
Tanaka <f.tanaka@gftd.works>
2023-05-18 14:04:47 +0900

    initial commit

---
+++ todo-app/resources/js/app.jsx
@@ -0,0 +1,13 @@
+import './bootstrap';
+import ReactDOM from "react-dom/client";
+
+function App() {
+    return (
+        <>
+            <h1>Hello World</h1>
+        </>
+    );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("app"));
+root.render(<App />);
