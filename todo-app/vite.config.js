98655dd35942737f4a7cabcb60b9f4aaa49c06a3
Tanaka <f.tanaka@gftd.works>
2023-05-19 11:10:41 +0900

    react-router追加,フロントのトップをapp.jsxに変更(設計図は異なる)

--- todo-app/vite.config.js
+++ todo-app/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     plugins: [
         react(),
         laravel({
-            input: ['resources/css/app.css', 'resources/js/index.jsx'],
+            input: ['resources/css/app.css', 'resources/js/app.jsx'],
             refresh: true,
         }),
     ],
