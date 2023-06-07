424966a06e4e85807b76559115e258b944cca18b
Tanaka <f.tanaka@gftd.works>
2023-06-07 13:52:23 +0900

    ユーザ登録画面のレイアウトを、設計図に合わせた

--- src/resources/js/Pages/Auth/Register.jsx
+++ src/resources/js/Pages/Auth/Register.jsx
@@ -4,7 +4,7 @@ import InputError from '@/Components/InputError';
 import InputLabel from '@/Components/InputLabel';
 import PrimaryButton from '@/Components/PrimaryButton';
 import TextInput from '@/Components/TextInput';
-import { Head, Link, useForm } from '@inertiajs/react';
+import { Head, useForm } from '@inertiajs/react';
 
 export default function Register() {
     const { data, setData, post, processing, errors, reset } = useForm({
@@ -34,89 +34,83 @@ export default function Register() {
         <GuestLayout>
             <Head title="ユーザ登録" />
 
-            <div>
-                ユーザ登録
+            <div className='mx-5'>
+                <div className="text-4xl mt-8 mb-10">
+                    ユーザ登録
+                </div>
+
+                <form onSubmit={submit}>
+                    <div>
+                        <InputLabel className='text-xl' htmlFor="name" value="氏名" />
+                        <TextInput
+                            id="name"
+                            name="name"
+                            value={data.name}
+                            className="mt-1 block w-full text-xl"
+                            autoComplete="name"
+                            isFocused={true}
+                            onChange={handleOnChange}
+                            required
+                            placeholder="Add text"
+                        />
+                        <InputError message={errors.name} className="mt-2" />
+                    </div>
+
+                    <div className="mt-6">
+                        <InputLabel className='text-xl' htmlFor="email" value="メールアドレス" />
+                        <TextInput
+                            id="email"
+                            type="email"
+                            name="email"
+                            value={data.email}
+                            className="mt-1 block w-full text-xl"
+                            autoComplete="username"
+                            onChange={handleOnChange}
+                            required
+                            placeholder="Add text"
+                        />
+                        <InputError message={errors.email} className="mt-2" />
+                    </div>
+
+                    <div className="mt-6">
+                        <InputLabel className='text-xl' htmlFor="password" value="パスワード" />
+                        <TextInput
+                            id="password"
+                            type="password"
+                            name="password"
+                            value={data.password}
+                            className="mt-1 block w-full text-xl"
+                            autoComplete="new-password"
+                            onChange={handleOnChange}
+                            required
+                            placeholder="Add text"
+                        />
+                        <InputError message={errors.password} className="mt-2" />
+                    </div>
+
+                    <div className="mt-6">
+                        <InputLabel className='text-xl' htmlFor="password_confirmation" value="パスワードを再入力" />
+                        <TextInput
+                            id="password_confirmation"
+                            type="password"
+                            name="password_confirmation"
+                            value={data.password_confirmation}
+                            className="mt-1 block w-full text-xl"
+                            autoComplete="new-password"
+                            onChange={handleOnChange}
+                            required
+                            placeholder="Add text"
+                        />
+                        <InputError message={errors.password_confirmation} className="mt-2" />
+                    </div>
+
+                    <div className="flex items-center justify-end my-10">
+                        <PrimaryButton className="w-48" disabled={processing}>
+                            <span className='text-lg m-auto leading-10'>登録</span>
+                        </PrimaryButton>
+                    </div>
+                </form>
             </div>
-
-            <form onSubmit={submit}>
-                <div>
-                    <InputLabel htmlFor="name" value="氏名" />
-
-                    <TextInput
-                        id="name"
-                        name="name"
-                        value={data.name}
-                        className="mt-1 block w-full"
-                        autoComplete="name"
-                        isFocused={true}
-                        onChange={handleOnChange}
-                        required
-                        placeholder="Add text"
-                    />
-
-                    <InputError message={errors.name} className="mt-2" />
-                </div>
-
-                <div className="mt-4">
-                    <InputLabel htmlFor="email" value="メールアドレス" />
-
-                    <TextInput
-                        id="email"
-                        type="email"
-                        name="email"
-                        value={data.email}
-                        className="mt-1 block w-full"
-                        autoComplete="username"
-                        onChange={handleOnChange}
-                        required
-                        placeholder="Add text"
-                    />
-
-                    <InputError message={errors.email} className="mt-2" />
-                </div>
-
-                <div className="mt-4">
-                    <InputLabel htmlFor="password" value="パスワード" />
-
-                    <TextInput
-                        id="password"
-                        type="password"
-                        name="password"
-                        value={data.password}
-                        className="mt-1 block w-full"
-                        autoComplete="new-password"
-                        onChange={handleOnChange}
-                        required
-                        placeholder="Add text"
-                    />
-
-                    <InputError message={errors.password} className="mt-2" />
-                </div>
-
-                <div className="mt-4">
-                    <InputLabel htmlFor="password_confirmation" value="パスワードを再入力" />
-
-                    <TextInput
-                        id="password_confirmation"
-                        type="password"
-                        name="password_confirmation"
-                        value={data.password_confirmation}
-                        className="mt-1 block w-full"
-                        autoComplete="new-password"
-                        onChange={handleOnChange}
-                        required
-                        placeholder="Add text"
-                    />
-
-                    <InputError message={errors.password_confirmation} className="mt-2" />
-                </div>
-
-                <div className="flex items-center justify-end mt-4">
-                    <PrimaryButton className="ml-4" disabled={processing}>
-                        登録
-                    </PrimaryButton>
-                </div>
-            </form>
         </GuestLayout>
     );
 }
