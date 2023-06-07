dbb9bf05841feffb6b9cac376b2ffb8f6276d26f
Tanaka <f.tanaka@gftd.works>
2023-06-07 11:16:36 +0900

    パスワードの再設定申し込みフォームのレイアウトを、設計図に合わせた

--- src/resources/js/Pages/Auth/ForgotPassword.jsx
+++ src/resources/js/Pages/Auth/ForgotPassword.jsx
@@ -3,6 +3,8 @@ import InputError from '@/Components/InputError';
 import PrimaryButton from '@/Components/PrimaryButton';
 import TextInput from '@/Components/TextInput';
 import { Head, useForm } from '@inertiajs/react';
+import InputLabel from '@/Components/InputLabel';
+import SecondaryButton from '@/Components/SecondaryButton';
 
 export default function ForgotPassword({ status }) {
     const { data, setData, post, processing, errors } = useForm({
@@ -23,36 +25,39 @@ export default function ForgotPassword({ status }) {
         <GuestLayout>
             <Head title="Forgot Password" />
 
-            <div className="mb-4 text-sm text-gray-600">
-                パスワードの再設定
-            </div>
-
-            <div>
-                メールアドレス
-            </div>
-
-            {status && <div className="mb-4 font-medium text-sm text-green-600">{status}</div>}
-
-            <form onSubmit={submit}>
-                <TextInput
-                    id="email"
-                    type="email"
-                    name="email"
-                    value={data.email}
-                    className="mt-1 block w-full"
-                    isFocused={true}
-                    onChange={onHandleChange}
-                    placeholder="テキストを追加"
-                />
-
-                <InputError message={errors.email} className="mt-2" />
-
-                <div className="flex items-center justify-end mt-4">
-                    <PrimaryButton className="ml-4" disabled={processing}>
-                        パスワード再設定リンクを送信
-                    </PrimaryButton>
+            <div className='mx-5'>
+                <div className="text-4xl mt-8 mb-10">
+                    パスワードの再設定
                 </div>
-            </form>
+
+                {status && <div className="mb-4 font-medium text-sm text-green-600">{status}</div>}
+
+                <form onSubmit={submit}>
+                    <div className='flex justify-between items-center'>
+                        <InputLabel className='text-xl' htmlFor="email" value="メールアドレス" />
+                        <TextInput
+                            id="email"
+                            type="email"
+                            name="email"
+                            value={data.email}
+                            className="block w-96 text-xl"
+                            isFocused={true}
+                            onChange={onHandleChange}
+                            placeholder="テキストを追加"
+                        />
+                    </div>
+                    <InputError message={errors.email} className="mt-2" />
+
+                    <PrimaryButton 
+                        className="mt-20 mb-14 border-solid border-2 border-blue-400 w-full" 
+                        disabled={processing}
+                    >
+                        <div className='text-lg leading-10 p-1 w-full'>
+                            パスワード再設定リンクを送信
+                        </div>
+                    </PrimaryButton>
+                </form>
+            </div>
         </GuestLayout>
     );
 }
