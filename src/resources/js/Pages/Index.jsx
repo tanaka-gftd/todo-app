ac2bb239ae709c7c211475d22c6f1f07cdcfba46
Tanaka <f.tanaka@gftd.works>
2023-06-08 15:39:44 +0900

    パスがない場合の仮ページを用意(LaravelのWelcomeテンプレートを置き換えた)

---
+++ src/resources/js/Pages/Index.jsx
@@ -0,0 +1,39 @@
+import { Link, Head } from '@inertiajs/react';
+
+export default function Index(props) {
+  return (
+    <>
+        <Head title="TODOアプリ(仮)" />
+
+        <div className="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
+            {props.auth.user ? (
+                <Link
+                    href={route('dashboard')}
+                    className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                >
+                    Dashboard
+                </Link>
+              ) : (
+                <>
+                  <Link
+                      href={route('login')}
+                      className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                  >
+                      Log in
+                  </Link>
+
+                  <Link
+                      href={route('register')}
+                      className="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                  >
+                      Register
+                  </Link>
+                </>
+              )
+            }
+        </div>
+    </>
+  )
+}
+
+
