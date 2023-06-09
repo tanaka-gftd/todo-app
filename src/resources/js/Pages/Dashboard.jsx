6857eea1e59f7076920307a9a077a871ad3fc871
Tanaka <f.tanaka@gftd.works>
2023-06-09 11:18:51 +0900

    設計図に合わせて、Dashboard.jsxから「ダッシュボード」の文言を表示するエリアを削除、ダッシュボードページのタイトルをタスクページに変更

--- src/resources/js/Pages/Dashboard.jsx
+++ src/resources/js/Pages/Dashboard.jsx
@@ -6,9 +6,8 @@ export default function Dashboard(props) {
         <AuthenticatedLayout
             auth={props.auth}
             errors={props.errors}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
         >
-            <Head title="Dashboard" />
+            <Head title="タスクページ" />
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
