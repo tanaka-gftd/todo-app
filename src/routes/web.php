d4e423f7f10c5f6950de2d0eb6a752a85205d831
Tanaka <f.tanaka@gftd.works>
2023-06-09 10:29:37 +0900

    Revert "パスがない場合の仮ページを用意(LaravelのWelcomeテンプレートを置き換えた)"
    This reverts commit ac2bb239ae709c7c211475d22c6f1f07cdcfba46.

--- src/routes/web.php
+++ src/routes/web.php
@@ -17,7 +17,7 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Index', [
+    return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
