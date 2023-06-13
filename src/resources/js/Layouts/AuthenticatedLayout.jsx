1275e8db9d657a7f270f2cc5b412148a14f6ece8
Tanaka <f.tanaka@gftd.works>
2023-06-13 15:18:37 +0900

    ダッシュボードページのアカウント名とアイコンの間を広げた、spanタグをdivタグへ修正、空白行削除

--- src/resources/js/Layouts/AuthenticatedLayout.jsx
+++ src/resources/js/Layouts/AuthenticatedLayout.jsx
@@ -32,20 +32,19 @@ export default function Authenticated({ auth, header, children }) {
                         <div className="relative">
                             <Dropdown>
                                 <Dropdown.Trigger>
-                                    <span className="flex rounded-md items-center">
+                                    <div className="flex rounded-md items-center">
                                         <button
                                             type="button"
                                             className="inline-flex items-center  border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-black hover:text-gray-700 focus:outline-none transition ease-in-out duration-150 bg-black"
                                         >
-                                            <span className="text-4xl text-white">{auth.user.name}さん</span>
+                                            <span className="text-4xl text-white mr-5">{auth.user.name}さん</span>
                                         </button>
                                         <button type="button" className='bg-white rounded-full'>
                                             <svg xmlns="http://www.w3.org/2000/svg" height="4em" viewBox="0 0 512 512">
                                                 <path d="M399 384.2C376.9 345.8 335.4 320 288 320H224c-47.4 0-88.9 25.8-111 64.2c35.2 39.2 86.2 63.8 143 63.8s107.8-24.7 143-63.8zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256 16a72 72 0 1 0 0-144 72 72 0 1 0 0 144z"/>
                                             </svg>
                                         </button>
-                                        
-                                    </span>
+                                    </div>
                                 </Dropdown.Trigger>
 
                                 <Dropdown.Content>
