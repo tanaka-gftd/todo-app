efee466a36c195f34b49af0123dd53b385fe3226
Tanaka <f.tanaka@gftd.works>
2023-06-13 15:12:05 +0900

    検索窓が表示されるのは、パスが/dashboardの時だけにした

--- src/resources/js/Layouts/AuthenticatedLayout.jsx
+++ src/resources/js/Layouts/AuthenticatedLayout.jsx
@@ -1,7 +1,9 @@
 import Dropdown from '@/Components/Dropdown';
 import TextInput from '@/Components/TextInput';
+import { usePage } from '@inertiajs/react';
 
 export default function Authenticated({ auth, header, children }) {
+    const { url, component } = usePage();
     return (
         <div className="min-h-screen bg-gray-100">
             <nav className="border-b border-gray-100 bg-black">
@@ -19,7 +21,7 @@ export default function Authenticated({ auth, header, children }) {
                                 <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/>
                             </svg>
                         </div>
-                        <div className='w-full'>
+                        <div className={url === '/dashboard' ? 'w-full' : 'hidden'}>
                             <TextInput
                                 placeholder="テキストを追加"
                                 className="rounded-lg border-none pl-12 text-2xl leading-8 w-full"
