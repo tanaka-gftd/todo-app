e15f51b85ebf516b9a3e7a7b4b6119ffa8a53d4e
Tanaka <f.tanaka@gftd.works>
2023-06-16 16:45:35 +0900

    不要なインポートを削除

--- src/app/Http/Controllers/Auth/PasswordResetCompletedController.php
+++ src/app/Http/Controllers/Auth/PasswordResetCompletedController.php
@@ -3,14 +3,6 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Auth\Events\PasswordReset;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Password;
-use Illuminate\Support\Str;
-use Illuminate\Validation\Rules;
-use Illuminate\Validation\ValidationException;
 use Inertia\Inertia;
 use Inertia\Response;
 
