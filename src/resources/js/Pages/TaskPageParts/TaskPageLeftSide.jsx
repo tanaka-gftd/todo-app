64fc6055864d0c41f18687707667613bb4714c1e
Tanaka <f.tanaka@gftd.works>
2023-06-22 15:02:03 +0900

    タスク表示ページの左側エリアの見た目だけ作成

--- src/resources/js/Pages/TaskPageParts/TaskPageLeftSide.jsx
+++ src/resources/js/Pages/TaskPageParts/TaskPageLeftSide.jsx
@@ -1,5 +1,29 @@
+import SecondaryButton from '@/Components/SecondaryButton';
+
 export default function TaskPageLeftSide() {
     return (
-        <>タスクページの左側です</>
+        <div className='text-2xl'>
+            <div className='mb-8'>
+                <p className='py-4'>すべて</p>
+                <p className='py-4'>今日</p>
+                <p className='py-4'>次の7日間</p>
+            </div>
+
+            <div>
+                <ul>
+                    <li className='py-4'>リスト1</li>
+                    <li className='py-4'>リスト2</li>
+                </ul>
+                <SecondaryButton className='mt-4 mb-8 border-solid border-2 border-blue-500'>
+                    <p className='text-xl text-blue-700 mt-1'>＋</p>
+                    <p className='text-xl text-blue-700 mt-1'>リストを追加</p>
+                </SecondaryButton>
+            </div>
+            
+            <div>
+                <p className='py-4'>タグ1</p>
+                <p className='py-4'>タグ2</p>
+            </div>
+        </div>
     )
 }
