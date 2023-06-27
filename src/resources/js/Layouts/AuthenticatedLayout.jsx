241e78087090cc49c3ac2236b031aa8f7cf6a700
Tanaka <f.tanaka@gftd.works>
2023-06-27 12:23:50 +0900

    ログアウトボタンで使用するコンポーネントをデフォルトで用意されているものに置き換えた＆それに伴い不要になったボタンコンポーネント削除

--- src/resources/js/Layouts/AuthenticatedLayout.jsx
+++ src/resources/js/Layouts/AuthenticatedLayout.jsx
@@ -1,7 +1,7 @@
 import Dropdown from '@/Components/Dropdown';
 import TextInput from '@/Components/TextInput';
 import { usePage } from '@inertiajs/react';
-import ButtonWithoutButtonTag from '@/Components/ButtonWithoutButtonTag';
+import DangerButton from '@/Components/DangerButton';
 
 export default function Authenticated({ auth, header, children }) {
     const { url } = usePage();
@@ -59,9 +59,9 @@ export default function Authenticated({ auth, header, children }) {
                                         <p className='text-lg'>ダッシュボード</p>
                                     </Dropdown.Link>
                                     <Dropdown.Link href={route('logout')} method="post" as="button">
-                                        <ButtonWithoutButtonTag className='bg-red-600 w-full justify-center'>
+                                        <DangerButton className='w-full justify-center'>
                                             <p className='text-lg'>ログアウト</p>
-                                        </ButtonWithoutButtonTag>
+                                        </DangerButton>
                                     </Dropdown.Link>
                                 </Dropdown.Content>
                             </Dropdown>
