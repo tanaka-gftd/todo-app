3fc5c47495d3eadfe9ec23c1380b097c2de9ddd3
Tanaka <f.tanaka@gftd.works>
2023-07-11 15:36:39 +0900

    タスクページでは、バックエンド(DB)とのやり取り中にローディング画面を表示するようにした

--- src/resources/js/Pages/Dashboard.jsx
+++ src/resources/js/Pages/Dashboard.jsx
@@ -41,34 +41,41 @@ export default function Dashboard(props) {
         getTaskList();
     },[]);
     
-
-    return (
-        <AuthenticatedLayout
-            auth={props.auth}
-            errors={props.errors}
-        >
-            <Head title="タスクページ" />
-
-            <div className="py-12">
-                <div className="max-w-8xl mx-auto px-8 flex justify-between">
-                    <div className="p-8 bg-white shadow w-4/12">
-                        <TaskPageLeftSide 
-                            taskListFront={taskListFront}
-                            getTaskList={getTaskList}
-                        />
-                    </div>
-
-                    <div className="p-8 mx-8 bg-red-100 shadow w-4/12">
-                        <TaskPageCenter/>
-                    </div>
-
-                    <div className="p-8 bg-white shadow w-4/12">
-                        <TaskPageRightSide/>
+    //バックエンドとのやり取り中はローディング画面を挿入するようにしたいので、一旦タスクページ全体を変数に格納
+    const MainAria = () => {
+        return (
+            <AuthenticatedLayout
+                auth={props.auth}
+                errors={props.errors}
+            >
+                <Head title="タスクページ" />
+    
+                <div className="py-12">
+                    <div className="max-w-8xl mx-auto px-8 flex justify-between">
+                        <div className="p-8 bg-white shadow w-4/12">
+                            <TaskPageLeftSide 
+                                taskListFront={taskListFront}
+                                getTaskList={getTaskList}
+                            />
+                        </div>
+    
+                        <div className="p-8 mx-8 bg-red-100 shadow w-4/12">
+                            <TaskPageCenter/>
+                        </div>
+    
+                        <div className="p-8 bg-white shadow w-4/12">
+                            <TaskPageRightSide/>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </AuthenticatedLayout>
+        );
+    };
 
-            {isLoading ? ( <Loading/>):(null)} 
-        </AuthenticatedLayout>
+    //バックエンドとのやりとり中かどうかで、表示する画面を切り替える(ここで返されるのがDashBoardコンポーネント)
+    return (
+        <>
+            {isLoading ? ( <Loading/>):(<MainAria/>)} 
+        </>
     );
 }
