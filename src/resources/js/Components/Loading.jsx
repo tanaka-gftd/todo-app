742f1efc3199de8a581ea891ac84c3952499f981
Tanaka <f.tanaka@gftd.works>
2023-07-12 11:13:29 +0900

    ローディング画面のアイコンや背景色を変更

--- src/resources/js/Components/Loading.jsx
+++ src/resources/js/Components/Loading.jsx
@@ -1,18 +1,16 @@
-import { Oval } from 'react-loader-spinner';
+import { Circles } from 'react-loader-spinner';
 
 export default  function Loading(){
     return (
         <div 
-            className='absolute inset-0 top-0 h-full w-screen bg-gray-500/75 flex items-center justify-center z-50'
+            className='absolute top-0 h-full w-screen bg-gray-200 flex items-center justify-center'
         >
-            <Oval
-                ariaLabel="loading-indicator"
-                height={100}
-                width={100}
-                strokeWidth={5}
-                strokeWidthSecondary={1}
-                color="#3b82f6"
-                secondaryColor="#f3f4f6"
+            <Circles
+                height="120"
+                width="120"
+                color="rgb(59 130 246)"
+                ariaLabel="circles-loading"
+                visible={true}
             />
         </div>
     );
