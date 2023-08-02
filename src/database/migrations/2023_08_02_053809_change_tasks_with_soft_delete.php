3f2281fd6ccddd9d4e99c1c28418df1d71efad78
Tanaka <f.tanaka@gftd.works>
2023-08-02 14:48:12 +0900

    タスク削除を論理削除で行えるよう、マイグレーションファイルとモデルを設定

---
+++ src/database/migrations/2023_08_02_053809_change_tasks_with_soft_delete.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //tasksテーブルに論理削除機能を追加
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //ダウン時は、tasksテーブルに論理削除機能を削除
+        Schema::table('tasks', function (Blueprint $table) {
+            //Laravel5.8以降の書き方
+            $table->dropSoftDeletes();
+
+            /*
+                Laravel5.7以前は、deleted_atカラムを削除することで行う
+                $table->dropColumn('deleted_at'); 
+            */
+        });
+    }
+};
