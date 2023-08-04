e193bf167c0cd5bc0d7c67d56fc9e4116f93b41d
Tanaka <f.tanaka@gftd.works>
2023-08-04 14:46:59 +0900

    タスクの期限が迫るなどすると期限日の下に警告が表示されるようにした。時刻修正の処理の説明コメントを簡略化。完了済みタスクを表示している時は、タスク完了ボタンが表示されないようにした。タスク完了ボタンとタスク削除ボタンの位置を入れ替えた。

--- src/resources/js/Pages/TaskPageParts/TaskPageRightSide.jsx
+++ src/resources/js/Pages/TaskPageParts/TaskPageRightSide.jsx
@@ -47,10 +47,46 @@ export default function TaskPageRightSide(props) {
     };
 
 
+    //タスクの期限の警告表示用
+    //引数として、期限日の23:59:59を持つDateオブジェクトと、タスク完了済みかどうかの真偽値を受け取る
+    const Deadline = ({date, isDone}) => {
+
+        //今日の日付(JST)を取得し、UTCに修正
+        let now = new Date();
+        now.setHours(now.getHours() - 9);
+
+        //期限までのミリ秒を求める
+        let diff = date.getTime() - now.getTime();
+
+        //期限終了は、期限日の23時59分59秒なので
+        let count = (1000*60*60*23) + (1000*60*59) + (1000*59);
+
+        //1日をミリ秒で換算
+        let millisecondDay = 1000*60*60*24;
+
+        //タスク完了済みの場合
+        if(isDone){
+            return (<p className="text-xl mt-2 text-green-500">このタスクは完了しています。</p>)
+        };
+
+        //期限が過ぎた、期限が今日、期限が明日、それ以外、の４パターンで表示する警告を切り替える
+        //(switch構文ではレンダリング出来ない？ので、elseifで対応)
+        if(diff < 0){
+            return (<p className="text-xl mt-2 text-red-500">タスクの期限を過ぎています！!</p>)
+        } else if(diff <= count){
+            return (<p className="text-xl mt-2 text-red-500">タスクの期限は今日までです！</p>)
+        } else if(diff <= count + millisecondDay) {
+            return (<p className="text-xl mt-2 text-yellow-500">タスクの期限は明日までです！</p>)
+        } else {
+            return null;
+        };
+    };
+
+
     return (
         <>
             {!props.clickedTaskId ? 
-                (<p className='text-xl'>作成したタスクを選択すると、ここに詳細が表示されます</p>) 
+                (<p className='text-xl'>成したタスクを選択すると、ここに詳細が表示されます</p>) 
                 : 
                 props.tasks.map((value, index) => {
                     //右エリアに表示するタスクは、中央エリアでクリックされたタスクのIDと一致しているものだけとする
@@ -58,33 +94,19 @@ export default function TaskPageRightSide(props) {
                     if(value.task_id === props.clickedTaskId){
 
                         /* 
-                            以下2点の理由より、タスク期限の日時がズレる
-                                •inputタグのdateで送信できる日付はUTC。
-                                •本プロジェクトのタイムゾーンはJST。
+                            以下３点の理由より、タスク期限の日時がズレる
+                            •inputタグのdateで送信される日付は、UTC時間での日付。
+                            •DBからのデータ取り出し時に、プロジェクトのタイムゾーンに自動で修正される。
+                            •本プロジェクトのタイムゾーンはJST。
 
-                            (例)
-                            inputタグのdateで、2023年8月29日(UTC)を送信
-                            ↓
-                            DB側では送信された2023年8月29日を、2023-08-29で保管し、時刻部分は内部で午前0時00分00秒と保持。
-                            ↓
-                            DBからフロントにデータを持ってくる時は、プロジェクトのタイムゾーン(JST)に合わせて、自動で9時間足され、
-                            2023年8月29日午前9時となる。
-                            ↓
-                            期限が過ぎたかどうかのチェックに問題が生じる
-
-                            また、タスクの期限は、指定した期日が終わるまでにしたいので、ここも加工する必要がある。
-
-                            これらの加工は、DBからの抽出時にコントローラ側で行う方法もあるが、
-                            フロントでの加工の方が分かりやすいので、フロントで実施
+                            なので、タイムゾーンの違いによる時刻自動修正でずれた時刻を、再度修正する必要がある。
+                            また、タスク期限は、inputタグのdateで送信した日が終わるまでにする。
                         */
-                        let newDeadline = new Date(value.deadline);
-
-                        //タイムゾーンの違いによる時刻自動修正でずれた時刻を、再修正
-                        //タスク期限は、inputタグのdateで送信じた日が終わるまでにする。
-                        newDeadline.setHours(newDeadline.getHours() - 9);
-                        newDeadline.setHours(23);
-                        newDeadline.setMinutes(59);
-                        newDeadline.setSeconds(59);
+                        let date = new Date(value.deadline);
+                        date.setHours(date.getHours() - 9);
+                        date.setHours(23);
+                        date.setMinutes(59);
+                        date.setSeconds(59);
 
                         return (
                             <div key={index}>
@@ -95,20 +117,21 @@ export default function TaskPageRightSide(props) {
                                     <p className="text-2xl mt-12">タスクのコメント</p>
                                     <p className="text-3xl mt-2">{value.comment}</p>
                                     <p className="text-2xl mt-12">タスクの期限</p>
-                                    <p className="text-3xl mt-2">{newDeadline.toLocaleDateString()}</p>
+                                    <p className="text-3xl mt-2">{date.toLocaleDateString()}</p>
+                                    <Deadline date={date}  isDone={value.is_done}/>
                                     <p className="text-2xl mt-12">タスクの優先度</p>
                                     <PriorityColor num={value.priority}/>
                                 </div>
                                 <div className='flex justify-between px-10'>
-                                    <form className='mt-20 my-10' onSubmit={submitDone}>
-                                        <PrimaryButton className="w-48" disabled={processing}>
-                                            <span className='text-lg m-auto leading-10'>タスク完了</span>
-                                        </PrimaryButton>
-                                    </form>
                                     <form className='mt-20 my-10' onSubmit={submitDelete}>
                                         <DangerButton className="w-48" disabled={processing}>
                                             <span className='text-lg m-auto leading-10'>タスクを削除</span>
                                         </DangerButton>
+                                    </form>
+                                    <form className='mt-20 my-10' onSubmit={submitDone} hidden={value.is_done}>
+                                        <PrimaryButton className="w-48" disabled={processing}>
+                                            <span className='text-lg m-auto leading-10'>タスク完了</span>
+                                        </PrimaryButton>
                                     </form>
                                 </div>
                             </div>
