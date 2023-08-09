cbb09746a0b93fe73fadbea99d559734b981e5dc
Tanaka <f.tanaka@gftd.works>
2023-08-09 10:59:29 +0900

    期限が迫っているタスクを、モーダルウィンドウで通知するようにした

--- src/resources/js/Pages/Dashboard.jsx
+++ src/resources/js/Pages/Dashboard.jsx
@@ -36,9 +36,44 @@ export default function Dashboard(props) {
         setShowNotificationModalFlag(false);
     };
 
+    //タスクの期限通知のために、タスクIDを保管する配列
+    const over = [];  //期限過ぎ
+    const justBefore = [];  //期限直前
+    const soon = [];  //そろそろ期限
+
+    //タスク期限チェック
+    tasks.map((value) => {
+        const deadline = new Date(value.deadline);  //期限日時のDateオブジェクト
+        const now = new Date();  //現在のDateオブジェクト
+        const diff = deadline.getTime() - now.getTime();  //現在から期限までのミリ秒を求める
+        const millisecondDay = 1000*60*60*24;  //1日をミリ秒で換算
+        
+        if(!value.is_done){  //未完了のタスクのみチェック
+            switch (true) {
+                case diff < 0:  //期限過ぎ
+                    over.push(value);
+                break;
+                case diff <= millisecondDay*1/2:  //期限まで残り12時間を切った
+                    justBefore.push(value)
+                break;
+                case diff <= millisecondDay:  //期限まで残り24時間を切った
+                    soon.push(value);
+                break;
+                default:
+                break;
+            };
+        };
+    });
+
 
     //バックエンドとのやり取り中はローディング画面に切り替わるようにしたいので、一旦タスクページ全体を変数に格納
     const MainAria = () => {
+
+        //タスクの期限の通知がない場合は、通知用モーダルを表示させない
+        if((over.length === 0)&&(justBefore.length === 0)&&(soon.length === 0)){
+            setShowNotificationModalFlag(false)
+        };
+
         return (
             <AuthenticatedLayout
                 auth={props.auth}
@@ -86,8 +121,51 @@ export default function Dashboard(props) {
                         <p onClick={()=>closeModal()} className="cursor-pointer absolute top-5 right-5 text-4xl">
                             ×
                         </p>
-                        <p className="text-2xl">期限が迫っているタスクがあります。</p>
-                        <p className="text-2xl">以下のタスクを確認してください。</p>
+                        <div className="text-2xl">
+                            <p className="mb-8 text-2xl">タスクの期限についての通知があります</p>
+                            <div style={{display: over.length === 0 ? 'none' : ''}}>
+                                <p className='mt-4'>期限を過ぎたタスク</p>
+                                <ul className='border-b-2 border-neutral-400'>
+                                    {over.map((value, index)=>{
+                                        return (
+                                            <li key={index} className='mt-2'>
+                                                <p className="text-xl ml-4">リスト名：{value.task_list_title}</p>
+                                                <p className="text-xl ml-10">タスク名：{value.task_name} </p>
+                                                <p className="text-xl ml-10">期限：{value.deadline} </p>
+                                            </li>
+                                        );
+                                    })}
+                                </ul>
+                            </div>
+                            <div style={{display: justBefore.length === 0 ? 'none' : ''}}>
+                                <p className='mt-4'>期限直前のタスク（期限まで12時間以下）</p>
+                                <ul className='border-b-2 border-neutral-400'>
+                                    {justBefore.map((value, index)=>{
+                                        return (
+                                            <li key={index} className='mt-2'>
+                                                <p className="text-xl ml-4">リスト名：{value.task_list_title}</p>
+                                                <p className="text-xl ml-10">タスク名：{value.task_name} </p>
+                                                <p className="text-xl ml-10">期限：{value.deadline} </p>
+                                            </li>
+                                        );
+                                    })}
+                                </ul>
+                            </div>
+                            <div style={{display: soon.length === 0 ? 'none' : ''}}>
+                                <p className='mt-4'>そろそろ期限のタスク（期限まで24時間以下）</p>
+                                <ul className='border-b-2 border-neutral-400'>
+                                    {soon.map((value, index)=>{
+                                        return (
+                                            <li key={index} className='mt-2'>
+                                                <p className="text-xl ml-4">リスト名：{value.task_list_title}</p>
+                                                <p className="text-xl ml-10">タスク名：{value.task_name} </p>
+                                                <p className="text-xl ml-10">期限：{value.deadline} </p>
+                                            </li>
+                                        );
+                                    })}
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </Modal>
             </AuthenticatedLayout>
