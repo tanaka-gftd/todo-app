5f173786bbd37863a2a82bbf58604ac4e37c98ca
Tanaka <f.tanaka@gftd.works>
2023-08-15 14:11:09 +0900

    tasksテーブルにタグも登録できるようにした

--- src/app/Http/Controllers/TaskController.php
+++ src/app/Http/Controllers/TaskController.php
@@ -14,8 +14,30 @@ class TaskController extends Controller
         //入力された文字列のチェック
         $request->validate([
             'taskName' => 'required|string|max:255',
-            'comment' => 'required|string|max:255'
+            'comment' => 'required|string|max:255',
+            'tagsArray' => 'array',
+            'tagsArray.*' => 'string|max:255'
         ]);
+
+        //フロントから送られてきたタグを保管する変数を用意
+        //tasksテーブルのtag_id_1カラムとtag_id_2カラムは、どちらもnullでもOKなので、値はnullで宣言
+        $tag_1 = null;
+        $tag_2 = null;
+
+        //フロントから送られてきたタグを保存した配列を、一旦別の変数に保存
+        //この配列は、空配列、要素数1、要素数2、の3パターンのうちのどれかとなる
+        $tagsArray = $request->tagsArray;
+
+        //フロントから送られてきたタグを、タグを保存している配列の要素数に応じて変数に保存(nullを上書きする)
+        //下記のpriorityと同様の理由で、念の為intvalで数値型にキャスト
+        //なお、$tag_1がnullで、$tag_2に数値が入るようなパターンは無しとする
+        if(count($tagsArray) === 2){
+            $tag_1 = intval($tagsArray[0]);
+            $tag_2 = intval($tagsArray[1]);
+        } 
+        if(count($tagsArray) === 1){
+            $tag_1 = intval($tagsArray[0]);
+        }
 
         //tasksテーブルに、レコード登録
         Task::create([
@@ -31,6 +53,10 @@ class TaskController extends Controller
             //フロントから送られてくる$request->priorityは文字列型
             //DB登録時に自動で数値型に変換されるようだが、念の為intvalでキャストしておく
             'priority' => intval($request->priority), 
+
+            //タグを登録
+            'tag_id_1' => $tag_1,
+            'tag_id_2' => $tag_2
         ]);
     }
 
