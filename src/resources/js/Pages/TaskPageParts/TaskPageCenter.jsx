d9764e7ac942d0ec25ec688a5a8c60d840000fc4
Tanaka <f.tanaka@gftd.works>
2023-08-23 11:01:22 +0900

    タスクのコメントの最大文字数を100字までに制限(フロント側、バックエンド側共に設定)

--- src/resources/js/Pages/TaskPageParts/TaskPageCenter.jsx
+++ src/resources/js/Pages/TaskPageParts/TaskPageCenter.jsx
@@ -244,6 +244,7 @@ export default function TaskPageCenter(props) {
                                 className="text-2xl w-full leading-5 py-4 my-5"
                                 placeholder="コメント"
                                 onChange={handleOnChange}
+                                maxLength="100"
                                 required
                             />
 
