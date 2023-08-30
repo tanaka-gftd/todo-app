8f0abb1e0a1eb7812f0f1e66f9bd5ac1022d9de6
Tanaka <f.tanaka@gftd.works>
2023-08-30 10:41:47 +0900

    Password forms should have (optionally hidden) username fields for accessibilityとのことなので、非表示のemailフィールドを追加

--- src/resources/js/Pages/Profile/Partials/UpdatePasswordForm.jsx
+++ src/resources/js/Pages/Profile/Partials/UpdatePasswordForm.jsx
@@ -3,17 +3,24 @@ import InputError from '@/Components/InputError';
 import InputLabel from '@/Components/InputLabel';
 import PrimaryButton from '@/Components/PrimaryButton';
 import TextInput from '@/Components/TextInput';
-import { useForm } from '@inertiajs/react';
+import { useForm, usePage } from '@inertiajs/react';
 import { Transition } from '@headlessui/react';
 
 export default function UpdatePasswordForm({ className }) {
     const passwordInput = useRef();
     const currentPasswordInput = useRef();
+    const user = usePage().props.auth.user;
 
     const { data, setData, errors, put, reset, processing, recentlySuccessful } = useForm({
         current_password: '',
         password: '',
         password_confirmation: '',
+        emailInUpdatePasswordSection: user.email
+        /* 
+            dataの持つプロパティは一意である必要があるため、
+            同じページに表示される他のセクション内のdateプロパティと被らないようにする必要がある。
+            なので、email: user.email ではNGとなる。
+        */
     });
 
     const updatePassword = (e) => {
@@ -39,6 +46,21 @@ export default function UpdatePasswordForm({ className }) {
     return (
         <section className={className}>
             <form onSubmit={updatePassword} className="space-y-6">
+                <div className='hidden'>
+                    <div>
+                        <InputLabel className='text-xl' htmlFor="emailInUpdatePasswordSection" value="メールアドレス" />
+
+                        <TextInput
+                            id="emailInUpdatePasswordSection"
+                            value={data.emailInUpdatePasswordSection}
+                            type="email"
+                            className="mt-1 block w-full text-xl"
+                            autoComplete="emailInUpdatePasswordSection"
+                            readOnly
+                        />
+                    </div>
+                </div>
+
                 <div>
                     <InputLabel className='text-xl' htmlFor="current_password" value="現在のパスワード" />
 
